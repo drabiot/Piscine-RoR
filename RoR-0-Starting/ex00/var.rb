0616d27cc16a738a33a406c3ba45353a4d2b43df
drabiot <tchartie@student.42angouleme.fr>
2026-06-04 17:47:11 +0200

    Finish first ruby function to print content & type of a variable

---
+++ RoR-0-Starting/ex00/var.rb
@@ -0,0 +1,14 @@
+def my_var
+	a = 10
+	b = "10"
+	c = nil
+	d = 10.0
+
+	puts "my variables :"
+	puts "\ta contains: #{a} and is a type: #{a.class}"
+	puts "\tb contains: #{b} and is a type: #{b.class}"
+	puts "\tc contains: #{c.inspect} and is a type: #{c.class}"
+	puts "\td contains: #{d} and is a type: #{d.class}"
+end
+
+my_var
