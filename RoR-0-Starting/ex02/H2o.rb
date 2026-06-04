291d18b3367a170b34b056c23947c7f024aea265
drabiot <tchartie@student.42angouleme.fr>
2026-06-04 19:57:04 +0200

    Clean & Start working on the hashing for ex02

---
+++ RoR-0-Starting/ex02/H2o.rb
@@ -0,0 +1,32 @@
+def hashing(array)
+	hashed = Hash.new
+	
+	array.each do |name, score|
+		hashed[name] = score
+	end
+
+	puts hashed
+end
+
+def main
+	data = [['Caleb' , 24],
+			['Calixte' , 84],
+			['Calliste', 65],
+			['Calvin' , 12],
+			['Cameron' , 54],
+			['Camil' , 32],
+			['Camille' , 5],
+			['Can' , 52],
+			['Caner' , 56],
+			['Cantin' , 4],
+			['Carl' , 1],
+			['Carlito' , 23],
+			['Carlo' , 19],
+			['Carlos' , 26],
+			['Carter' , 54],
+			['Casey' , 2]]
+
+	hashing(data)
+end
+
+main
