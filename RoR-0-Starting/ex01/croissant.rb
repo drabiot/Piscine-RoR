de6f7480a6c30bbf0404f2f43b84e861b360c8b5
drabiot <tchartie@student.42angouleme.fr>
2026-06-05 13:16:21 +0200

    Protect ex01 & Finish ex03

--- RoR-0-Starting/ex01/croissant.rb
+++ RoR-0-Starting/ex01/croissant.rb
@@ -1,5 +1,11 @@
 def main
-	lines = File.readlines("numbers.txt", "r").join.split("\n").map { |l| l.delete(",").to_i}
+	file = "numbers.txt"
+	unless File.exist?(file)
+		puts "#{file} not found"
+		return
+	end
+
+	lines = File.readlines(file, "r").join.split("\n").map { |l| l.delete(",").to_i}
 	lines.sort!
 	for i in lines
 		puts i
---
+++ RoR-0-Starting/ex03/Where.rb
@@ -0,0 +1,29 @@
+def search_capitals(state)
+	states = {
+		"Oregon" => "OR",
+		"Alabama" => "AL",
+		"New Jersey" => "NJ",
+		"Colorado" => "CO"
+	}
+	capitals_cities = {
+		"OR" => "Salem",
+		"AL" => "Montgomery",
+		"NJ" => "Trenton",
+		"CO" => "Denver"
+	}
+
+	capital = capitals_cities[states[state]]
+	if capital
+		puts capital
+	else
+		puts "Unknown state"
+	end
+end
+
+def main
+	if ARGV.length() == 1
+		search_capitals(ARGV[0])
+	end
+end
+
+main
