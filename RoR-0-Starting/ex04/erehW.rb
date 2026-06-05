7e98bbe5a77b64829358eeca7263322387dfaeb5
drabiot <tchartie@student.42angouleme.fr>
2026-06-05 13:19:29 +0200

    Finish ex04

---
+++ RoR-0-Starting/ex04/erehW.rb
@@ -0,0 +1,29 @@
+def search_states(capital)
+	states = {
+		"Oregon" => "OR",
+		"Alabama" => "AL",
+		"New Jersey" => "NJ",
+		"Colorado" => "CO"
+	}
+	capitals_cities = {
+		"OR" => "Salem",
+		"AL" => "Montgomery",
+		"NJ" => "Trenton",
+		"CO" => "Denver"
+	}
+
+	state = states.key(capitals_cities.key(capital))
+	if state
+		puts state
+	else
+		puts "Unknown capital city"
+	end
+end
+
+def main
+	if ARGV.length() == 1
+		search_states(ARGV[0])
+	end
+end
+
+main
