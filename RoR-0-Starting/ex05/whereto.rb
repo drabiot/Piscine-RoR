3bf0ec83ea29ecfb3d0d67c070f19d5c7404740b
drabiot <tchartie@student.42angouleme.fr>
2026-06-05 13:34:46 +0200

    Start ex05

---
+++ RoR-0-Starting/ex05/whereto.rb
@@ -0,0 +1,32 @@
+def search_capitals(arg)
+	states = {
+		"Oregon" => "OR",
+		"Alabama" => "AL",
+		"New Jersey" => "NJ",
+		"Colorado" => "CO"
+	}
+	capitals_cities = {
+		"OR" => "Salem",
+		"AL" => "Montgomery",
+		"NJ" => "Trenton",
+		"CO" => "Denver"
+	}
+
+	capital = capitals_cities[states[arg]]
+	state = states.key(capitals_cities.key(arg))
+	if capital
+		puts "#{capital} is the capital of #{arg} (akr: #{states[arg]})"
+	elsif state
+		puts "#{arg} is the capital of #{state} (akr: #{capitals_cities.key(arg)})"
+	else
+		puts "#{arg} is neither a capital city nor a state"
+	end
+end
+
+def main
+	if ARGV.length() == 1
+		search_capitals(ARGV[0])
+	end
+end
+
+main
