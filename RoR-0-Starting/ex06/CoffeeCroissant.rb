3106d0e5fbea510cca48bbea157fd8787d182ab2
drabiot <tchartie@student.42angouleme.fr>
2026-06-05 13:55:50 +0200

    Finish ex06

---
+++ RoR-0-Starting/ex06/CoffeeCroissant.rb
@@ -0,0 +1,25 @@
+def hashing(array)
+	hash = Hash.new
+	
+	array.each do |name, score|
+		hash[name] = score
+	end
+
+	return hash
+end
+
+def main
+	data = [['Frank', 33],
+			['Stacy', 15],
+			['Juan' , 24],
+			['Dom' , 32],
+			['Steve', 24],
+			['Jill' , 24]]
+
+	hash = hashing(data)
+	sorted_hash = hash.sort_by { |key, value| [value, key] }
+
+	sorted_hash.each { |key, value| puts "#{key}" }
+end
+
+main
