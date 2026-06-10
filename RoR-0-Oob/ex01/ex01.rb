4a78715c72972f654e478965bdd0d026bc99463b
drabiot <tchartie@student.42angouleme.fr>
2026-06-10 13:34:55 +0200

    Add Tester

--- RoR-0-Oob/ex01/ex01.rb
+++ RoR-0-Oob/ex01/ex01.rb
@@ -39,9 +39,46 @@ class Html
 end
 
 if $PROGRAM_NAME == __FILE__
+	File.delete("test.html") if File.exist?("test.html")
+	puts "\e[32mBasic Test for Creation\e[0m"
+
 	a = Html.new("test")
 	10.times { |x| a.dump("titi_number#{x}") }
 	a.finish
 
 	puts File.read("test.html")
+	puts "\n"
+
+	begin
+		puts "\e[32mSame name Error\e[0m"
+		b = Html.new("test")
+		10.times { |x| b.dump("titi_number#{x}") }
+		b.finish
+	rescue RuntimeError => e
+		puts "\e[31mRaised Error : #{e.message}\e[0m\n\n"
+	end
+
+	begin
+		puts "\e[32mDump when body is closed\e[0m"
+		c = Html.new("test2")
+		10.times { |x| c.dump("titi_number#{x}") }
+		c.finish
+		10.times { |x| c.dump("titi_number#{x}") }
+	rescue RuntimeError => e
+		puts "\e[31mRaised Error : #{e.message}\e[0m\n\n"
+	ensure
+		File.delete("test2.html") if File.exist?("test2.html")
+	end
+
+	begin
+		puts "\e[32mClose the body when body already closed\e[0m"
+		d = Html.new("test3")
+		10.times { |x| d.dump("titi_number#{x}") }
+		d.finish
+		d.finish
+	rescue RuntimeError => e
+		puts "\e[31mRaised Error : #{e.message}\e[0m\n\n"
+	ensure
+		File.delete("test3.html") if File.exist?("test3.html")
+	end
 end
